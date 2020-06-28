4bd92f7675de15627a7ab4bfede946cd8174e329
ak8741096 <ak8741096@gmail.com>
2020-06-28 17:21:04 +0500

    added the themes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <ThemeSwitcher theme = 2 />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ThemeSwitcher from './components/ThemeSwitcher.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ThemeSwitcher
   }
 }
 </script>
---
+++ src/components/ThemeSwitcher.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="theme-container">
+    <h1>This is theme number {{ themes }} </h1>
+    <ul >
+        <li>Theme div here</li>
+    </ul>
+</div>
+    
+</template>
+
+<style scoped>
+    h1{
+        color: blueviolet;
+    }
+</style>
+
+<script>
+
+import * as themes from '../assets/theme.json'
+export default {
+    name: 'ThemeSwitcher',
+    props:{
+        theme: Number,
+        themes: themes
+    }
+}
+</script>
