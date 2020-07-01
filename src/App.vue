eff2bd4cda927a09cb47bdd1286fb6ed502b01bf
ak8741096 <ak8741096@gmail.com>
2020-07-01 09:38:30 +0500

    Color theme switcher functionality completed

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <ThemeSwitcher theme = 2 />
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ThemeSwitcher  :themes = "themes" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import json from './assets/theme.json'
 import ThemeSwitcher from './components/ThemeSwitcher.vue'
+
 
 export default {
   name: 'App',
+   data(){
+    return{
+      themes : json
+    }
+  },
   components: {
-    HelloWorld,
     ThemeSwitcher
   }
+ 
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root{
+  --primary: #ffd803;
+  --secondary: #e3f6f5;
+  --tertiary: #bae8e8;
+  --headline: #272343;
+  --buttonText: #272343;
+  --paragraphText:#2d334a;
+  --background: #fffffe;
+}
+body{
+  background-color: var(--background);
+}
+h1{
+  color: var(--headline);
+}
+p{
+  color: var(--paragraphText);
+}
+button{
+  color: var(--buttonText);
+  background-color: var(--primary); 
+  border: none;
+}
+
+button:focus{
+  outline: none;
+  border: 0.15rem solid var(--primary);
+}
+
+.active{
+  border: 0.15rem solid var(--primary);
 }
 </style>
--- src/components/ThemeSwitcher.vue
+++ src/components/ThemeSwitcher.vue
@@ -1,27 +1,89 @@
 <template>
-<div class="theme-container">
-    <h1>This is theme number {{ themes }} </h1>
-    <ul >
-        <li>Theme div here</li>
-    </ul>
+<div class="theme-switcher">
+    <div class="theme-container">
+        <button @click.prevent="changeTheme" class="theme" v-for="(theme,index) in themes" :key="theme.id" :data-index="index" :style="{backgroundColor: theme.colors.background}">
+            <span class="theme-name" :style="{color: theme.colors.headline}">{{theme.name}}</span>
+            <div class="theme-color-container">
+                <div class="theme-colors" :style="{backgroundColor: theme.colors.primary}"></div>
+                <div class="theme-colors" :style="{backgroundColor: theme.colors.secondary}"></div>
+                <div class="theme-colors" :style="{backgroundColor: theme.colors.tertiary}"></div>
+                <div class="theme-colors" :style="{backgroundColor: theme.colors.headline}"></div>
+            </div>  
+        </button>
+    </div>
+
+    <h1>{{title}}</h1>
+    <button>Click me</button>
 </div>
     
 </template>
 
 <style scoped>
-    h1{
-        color: blueviolet;
+    *{
+        margin:0;
+        padding:0;
+        box-sizing: border-box;
+    }
+    .theme-container{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        overflow-x: auto;
+    }
+    .theme-name{
+        flex-basis: 100%;
+        padding: 0.2rem 0;
+    }
+    .theme{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 0.5rem 0rem 0.75rem 0rem;
+        margin: 0 1rem 0 0;
+        border-radius: 0.5rem;
+        flex-wrap: wrap;
+    }
+    .theme-color-container{
+        display: flex;
+    }
+    .theme-colors{
+        width: 1.25rem;
+        height: 1.25rem;
+        border-radius: 50%;
+        border: 0.15rem solid #fff;
+        margin: 0 0 0 -0.2rem;
     }
 </style>
 
 <script>
-
-import * as themes from '../assets/theme.json'
 export default {
     name: 'ThemeSwitcher',
-    props:{
-        theme: Number,
-        themes: themes
+    data(){
+        return{
+            title: 'Colour theme switcher',
+        }
+    },
+    props: {
+        themes: Array
+    },
+    methods:{
+        changeTheme: async function(e){
+            // Getting the colors of the theme from the props according to the theme button clicked
+           const newTheme = this.themes[parseInt(e.target.closest('button').getAttribute("data-index"))].colors
+            // Defining the root of the document for cleaner code
+           let root = document.documentElement
+
+            // Looping through the colors object and setting the styles in the root
+           const entries = Object.entries(newTheme)
+           for(let i = 0; i < entries.length; i++){
+               for(let j = 1; j < entries[i].length; j++){
+            
+                   root.style.setProperty(`--${entries[i][0]}`, `${entries[i][1]}`)
+                   
+               }
+           }
+           
+        }
     }
 }
 </script>
